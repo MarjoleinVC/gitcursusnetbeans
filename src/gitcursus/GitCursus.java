9bcd6e83dbddb80a5e7b6e3af1f454557c1d4194
marjolein.vancelst <marjolein.vancelst@PAUL-EI1VSCGA7S.vdabheverlee.intra>
2014-06-17 14:58:29 +0200

    modified

--- src/gitcursus/GitCursus.java
+++ src/gitcursus/GitCursus.java
@@ -17,6 +17,8 @@ public class GitCursus {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Hallo");
     }
     
 }
